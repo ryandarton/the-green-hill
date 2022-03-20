a70cdc33a5de68abe31ee89a5ef8f4006cba12d5
Ryan Darton <ryan@timemachinevideo.com>
2022-03-20 16:12:11 -0600

    Adds auth-gaurd service and implements on some urls.

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Router, CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
+import 'rxjs/add/operator/map'
+import { Observable } from 'rxjs';
+import { auth } from 'firebase';
+
+@Injectable()
+export class AuthGuardService implements CanActivate {
+
+  constructor(private auth: AuthService, private router: Router) { }
+
+  canActivate() {
+    return this.auth.user$.map(user => {
+      if (user) return true;
+
+      this.router.navigate(['/login']);
+      return false;
+    });
+  }
+
+}
