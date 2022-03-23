6a457294511a283e34b5b33d22f35979b38a559a
Ryan Darton <ryan@timemachinevideo.com>
2022-03-23 12:52:12 -0600

    Adds delete button and delete functionality.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -11,7 +11,7 @@ export class AdminProductsComponent implements OnInit {
 
   constructor(private productService: ProductService) {
     this.products$ = this.productService.getAll();
-   }
+  }
 
   ngOnInit() {
   }
--- src/app/admin/product-form/product-form.component.ts
+++ src/app/admin/product-form/product-form.component.ts
@@ -30,6 +30,14 @@ export class ProductFormComponent implements OnInit {
     this.router.navigate(['/admin/products']);
   }
 
+  delete() {
+    if (!confirm('Are you sure you want to delete this product?')) return;
+
+    this.productService.deleteProduct(this.id);
+    this.router.navigate(['/admin/products']);
+
+  }
+
   ngOnInit(): void {
   }
 
