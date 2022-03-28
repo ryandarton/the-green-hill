667daff47459df7712030a5dd84d76c7debb0394
Ryan Darton <ryan@timemachinevideo.com>
2022-03-28 13:31:49 -0600

    Adds search bar.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Product } from '../../models/product';
 import { ProductService } from '../../product.service';
 
 @Component({
@@ -6,11 +8,23 @@ import { ProductService } from '../../product.service';
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
-  products$;
+export class AdminProductsComponent implements OnInit, OnDestroy {
+  products: Product[];
+  filteredProducts: any[];
+  subscription: Subscription;
 
   constructor(private productService: ProductService) {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll().subscribe(products => this.filteredProducts = this.products = products);
+  }
+
+  filter(query: string) {
+    this.filteredProducts = (query) ?
+    this.products.filter(p => p.title.toLowerCase().includes(query.toLowerCase())) :
+    this.products;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   ngOnInit() {
